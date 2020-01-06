9ace4101907349f4959f06843454115ddc502717
Ladoui Abdellah <ladoui@gmail.com>
2020-01-06 11:09:20 +0100

    css: rename and cssnano done

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,9 @@ const { src, dest, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const cssnano = require('gulp-cssnano')
 
 const build = () => {
     console.log('build task')
@@ -12,7 +15,11 @@ const css = () => {
         .pipe(sourcemaps.init())
         .pipe(autoprefixer())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps/'))
+        .pipe(sourcemaps.write())
+        .pipe(dest('./dist/css/'))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(dest('./dist/css/'))
 }
 
